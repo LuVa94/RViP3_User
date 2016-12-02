1d011dc914fd68c809701896e8e9fee32d31864f
LuVa94 <LuVa94@users.noreply.github.com>
2016-12-02 15:11:00 +0400

    Add files via upload

---
+++ рвип3/рвип3/File.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace рвип3
+{
+    public class File : CollectionBase
+    {
+        ConcurrentBag<string> cb_file = new ConcurrentBag<string>();
+        ConcurrentBag<string> cb_state = new ConcurrentBag<string>();
+        //string stfile = "free";
+        string stfile = "";
+        const int slep = 4;
+
+        public void Get_info(string file, string state)
+        {
+            Console.WriteLine("Файловая система принимает: Имя файла " + file.ToString() + " для  " + state.ToString());
+            cb_file.Add(file.ToString());
+            cb_state.Add(state.ToString());
+        }
+
+        string c = "";
+        public string Change_state_file(string file, string stfile)
+        {
+            if ((stfile.StartsWith("note")))
+            {
+                    c = "busy";
+                }
+            return stfile;
+        }
+        string stat = "";
+        public string state_file(string file, string stfile)
+        {
+            //foreach (string sp in cb_file)
+            //{
+            //    foreach (string c in cb_state)
+            //    {
+            if ((stfile.StartsWith("read")))
+                    {
+                        stat = file.ToString()+" для чтения";
+                    }
+                    else
+                    {
+                        stat = file.ToString() + " для записи";
+                    }
+            //    }
+            //}
+            return stat;
+        }
+
+        public void Display_info()
+        {
+            foreach (string sp in cb_file)
+            {
+                foreach (string c in cb_state)
+                {
+                    if ((c == "read") && (stfile == "free"))
+                    {
+                        Console.WriteLine("чтение");
+                    }
+                    else
+                    {
+                        Console.WriteLine("ожидание");
+                    }
+                }
+            }
+        }
+
+        public void Send(string state)
+        {
+            int sl = 0;
+            Random r = new Random();
+            sl = r.Next(10, 100);
+            Thread.Sleep(sl * slep);
+            //sv.Get_info(state);
+        }
+    }
+}
---
+++ рвип3/рвип3/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Управление общими сведениями о сборке осуществляется с помощью 
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// связанные со сборкой.
+[assembly: AssemblyTitle("рвип3")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("рвип3")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Параметр ComVisible со значением FALSE делает типы в сборке невидимыми 
+// для COM-компонентов.  Если требуется обратиться к типу в этой сборке через 
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("6a64b4aa-a848-40b2-ac6b-a6d7a0c3d55e")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии 
+//   Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
